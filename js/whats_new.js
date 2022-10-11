5a14266498d436b05e99a69f23f0f2e42f6ebe87
hi100a <112834885+hi100a@users.noreply.github.com>
2022-10-11 16:12:06 +0900

    20221011
    로그인페이지추가

--- js/whats_new.js
+++ js/whats_new.js
@@ -1,4 +1,18 @@
-$(function() {
-  new Swiper('.swiper-container');
-//스와이프
+
+
+  //<!-- Initialize Swiper -->
+
+$(function(){
+
+
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 2.3,
+      spaceBetween: 10,
+      freeMode: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
 })

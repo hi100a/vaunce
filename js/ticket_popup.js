f191eec5f95f1b7a6f1dfa2519975d40708cbb06
hi100a <112834885+hi100a@users.noreply.github.com>
2022-10-20 16:10:27 +0900

    20221020-02
    ticket

--- js/ticket_popup.js
+++ js/ticket_popup.js
@@ -1,7 +1,16 @@
 $(function(){
-    $('#bot_bar .ticket').click(function(){
-        $('#ticket_popup').animate({right:0},500);
-        // $('#bot_bar .ticket_open').display({none})
+    state=0;
+    $('#bot_bar .ticket a').click(function(){
+       if ( state==0) {
+        $('#ticket_popup').animate({bottom:0},500);
+        $(this).addClass("ticket_close");
+        state=1;}
+        else{
+            $('#ticket_popup').animate({bottom:'-100vh'},500);
+            $(this).removeClass("ticket_close");
+            state=0;
+        } 
+               // $('#bot_bar .ticket_open').display({none})
         // $('#bot_bar .ticket_open').display({block})
     })
  
